b46eb420ce4033206edb1bf1e64e86df92f3bbcc
AluOscarMunoz <oscaroscar2k@gmail.com>
2026-01-09 13:47:53 +0100

    Add files via upload

---
+++ Ejercicios básicos de Powershell del 1 al 3 Oscar/Ejercicio2.ps1
@@ -0,0 +1,8 @@
+﻿$a = [int](Read-Host "Introduce el primer número")
+$b = [int](Read-Host "Introduce el segundo número")
+
+Write-Host "Suma: $($a + $b)"
+Write-Host "Resta: $($a - $b)"
+Write-Host "Multiplicación: $($a * $b)"
+Write-Host "División: $($a / $b)"
+Write-Host "Residuo: $($a % $b)"
---
+++ Ejercicios básicos de Powershell del 1 al 3 Oscar/Ejercicio3.ps1
@@ -0,0 +1,4 @@
+﻿$hora = [float](Read-Host "Horas trabajadas: ")
+$precio = [float](Read-Host "Coste por horas: ")
+
+Write-Host "Tu salario es $($hora * $precio)"
