9fed4093e1a1fa6deda0df2367d86ec93050d0f2
AluOscarMunoz <oscaroscar2k@gmail.com>
2026-02-01 11:59:13 +0100

    Add files via upload

---
+++ ejercicio1.ps1
@@ -0,0 +1,15 @@
+﻿$departamentos = Import-Csv "C:\Users\Administrador\Downloads\archivos\departamentos.csv" -Delimiter ";"
+$empleados = Import-Csv "C:\Users\Administrador\Downloads\archivos\empleados.csv" -Delimiter ";"
+
+New-ADOrganizationalUnit -Name "Empresa" -Path "DC=empresa,DC=local" -ErrorAction SilentlyContinue
+
+foreach ($d in $departamentos) {
+    New-ADOrganizationalUnit -Name $d.Departamento -Path "OU=Empresa,DC=empresa,DC=local" -ErrorAction SilentlyContinue
+    New-ADGroup -Name "$($d.Departamento)" -GroupScope Global -Path "OU=$($d.Departamento),OU=Empresa,DC=empresa,DC=local" -ErrorAction SilentlyContinue
+}
+
+foreach ($e in $empleados) {
+    $u = "$($e.Nombre.ToLower()).$($e.Apellido.ToLower())"
+    New-ADUser -Name "$($e.Nombre) $($e.Apellido)" -SamAccountName $u -UserPrincipalName "$u@empresa.local" -Path "OU=$($e.Departamento),OU=Empresa,DC=empresa,DC=local" -AccountPassword (ConvertTo-SecureString "aso2025." -AsPlainText -Force) -Enabled $true -ChangePasswordAtLogon $true -ErrorAction SilentlyContinue
+    Add-ADGroupMember -Identity "$($e.Departamento)" -Members $u -ErrorAction SilentlyContinue
+}
